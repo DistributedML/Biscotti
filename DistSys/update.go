813af5e12b889f7f8ed36c76f745342d762710e1
Shayan <shayan@naur.cs.ubc.ca>
2018-06-25 11:28:44 -0700

    Added update.go

---
+++ DistSys/update.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	// "encoding/binary"
+	// "bytes"
+)
+
+// Update - data object representing a single update
+type Update struct {
+	Iteration int
+	Delta     []float64
+}
+
+func (update Update) String() string {
+	return fmt.Sprintf("{Iteration:" + strconv.Itoa(update.Iteration) + ", " + "Deltas:" + arrayToString(update.Delta, ",") + "}")
+}
+
+func arrayToString(a []float64, delim string) string {
+	str := "[" + strings.Trim(strings.Replace(fmt.Sprint(a), " ", delim, -1), "[]") + "]"
+	return str
+
+}
+
+
