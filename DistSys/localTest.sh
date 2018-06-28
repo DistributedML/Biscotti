7ec72693e6e38a511f3f1113d6a3dead856d4b0f
Shayan <mshayanshafi@gmail.com>
2018-06-27 17:31:18 -0700

    Scalability temporary

--- DistSys/localTest.sh
+++ DistSys/localTest.sh
@@ -14,7 +14,6 @@ PID=`pgrep DistSys`
 while sudo kill $PID > /dev/null
 do
 	sudo kill -9 $PID
-
 	break
 done
 
@@ -26,15 +25,21 @@ go install
 
 echo "Running tests: No failure case. All nodes online"
 
-for (( totalnodes = 2; totalnodes < 5; totalnodes++ )); do
+for (( totalnodes = 40; totalnodes < 41; totalnodes++ )); do
 	
 	echo "Running with " $totalnodes "nodes"
 
 	for (( index = 0; index < totalnodes; index++ )); do
 		
 		thisLogFile=test1_$index\_$totalnodes.log
-		# sudo $GOPATH/bin/DistSys -i=$index -t=$totalnodes -d=creditcard > $thisLogFile & 
-		sudo $GOPATH/bin/DistSys -i=$index -t=$totalnodes -d=creditcard > $thisLogFile 2> /dev/null &
+		myAddress=127.0.0.1:
+		let thisPort=8000+$index
+		echo $index
+		echo $thisPort
+		echo $myAddress
+
+		sudo $GOPATH/bin/DistSys -i=$index -t=$totalnodes -d=creditcard -f=peersfile.txt -a=$myAddress -p=$thisPort > $thisLogFile & 
+		# sudo $GOPATH/bin/DistSys -i=$index -t=$totalnodes -d=creditcard > $thisLogFile 2> /dev/null &
 		if [ $index -eq 0 ] 
 		then			
 			sleep 10			
@@ -65,149 +70,149 @@ for (( totalnodes = 2; totalnodes < 5; totalnodes++ )); do
 	echo "SUCCESS! Nodes have same blockchain"
 
 done
-
-# #---------------------------------------------------Test 2: All nodes online except one---------------------------------------------------------------------------
-
-echo "Running tests: One node offline all the time"
-
-for (( totalnodes = 3; totalnodes < 5; totalnodes++ )); do
-	
-	echo "Running with " $totalnodes "nodes"
-
-	for (( index = 0; index < (totalnodes - 1); index++ )); do
 		
-		thisLogFile=test2_$index\_$totalnodes.log
-		# sudo $GOPATH/bin/DistSys -i=$index -t=$totalnodes -d=creditcard > $thisLogFile & 
-		sudo $GOPATH/bin/DistSys -i=$index -t=$totalnodes -d=creditcard > $thisLogFile 2> /dev/null &
-		if [ $index -eq 0 ] 
-		then			
-			sleep 10			
-		fi
-	done	
 
-	wait
+# # #---------------------------------------------------Test 2: All nodes online except one---------------------------------------------------------------------------
+
+# echo "Running tests: One node offline all the time"
+
+# for (( totalnodes = 3; totalnodes < 5; totalnodes++ )); do
 	
-	echo "Running with " $totalnodes "nodes complete. Testing similarity of blockchain"
+# 	echo "Running with " $totalnodes "nodes"
+
+# 	for (( index = 0; index < (totalnodes - 1); index++ )); do
+		
+# 		thisLogFile=test2_$index\_$totalnodes.log
+# 		# sudo $GOPATH/bin/DistSys -i=$index -t=$totalnodes -d=creditcard > $thisLogFile & 
+# 		sudo $GOPATH/bin/DistSys -i=$index -t=$totalnodes -d=creditcard > $thisLogFile 2> /dev/null &
+# 		if [ $index -eq 0 ] 
+# 		then			
+# 			sleep 10			
+# 		fi
+# 	done	
+
+# 	wait
 	
-	for (( outernode = 0; outernode < totalnodes-1; outernode++ )); do	
-		
-		outerLogFile=test2_$outernode\_$totalnodes.log
-		for (( innernode = 0; innernode < totalnodes-1 ; innernode++ )); do
-			innerLogFile=test2_$innernode\_$totalnodes.log			
-			if [ $innerLogFile == $outerLogFile ]; then
-				continue
-			fi
-			if !(cmp -s $innerLogFile $outerLogFile) 
-			then
-				echo Test failed: $innerLogFile and $outerLogFile are different
-				echo "Files are different"
-				exit -1	
-			fi		
-		done
-	done
-
-	echo "SUCCESS! Nodes have same blockchain"
-
-done
-
-#---------------------------------------------------Test 3: Nodes join at later iterations---------------------------------------------------------------------------
-
-echo "Running tests: Node joins at a later iteration"
-
-for (( totalnodes = 3; totalnodes < 5; totalnodes++ )); do
+# 	echo "Running with " $totalnodes "nodes complete. Testing similarity of blockchain"
 	
-	echo "Running with " $totalnodes "nodes"
-
-	for (( index = 0; index < (totalnodes); index++ )); do
+# 	for (( outernode = 0; outernode < totalnodes-1; outernode++ )); do	
 		
-		thisLogFile=test3_$index\_$totalnodes.log
-		# sudo $GOPATH/bin/DistSys -i=$index -t=$totalnodes -d=creditcard > $thisLogFile & 
-		sudo $GOPATH/bin/DistSys -i=$index -t=$totalnodes -d=creditcard > $thisLogFile 2> /dev/null &
-		if [ $index -le 1 ] 
-		then
-			sleep 10
-		else
-			sleep 50			
-		fi
-	done	
+# 		outerLogFile=test2_$outernode\_$totalnodes.log
+# 		for (( innernode = 0; innernode < totalnodes-1 ; innernode++ )); do
+# 			innerLogFile=test2_$innernode\_$totalnodes.log			
+# 			if [ $innerLogFile == $outerLogFile ]; then
+# 				continue
+# 			fi
+# 			if !(cmp -s $innerLogFile $outerLogFile) 
+# 			then
+# 				echo Test failed: $innerLogFile and $outerLogFile are different
+# 				echo "Files are different"
+# 				exit -1	
+# 			fi		
+# 		done
+# 	done
 
-	wait
-	echo "Running with " $totalnodes "nodes complete. Testing similarity of blockchain"
+# 	echo "SUCCESS! Nodes have same blockchain"
+
+# done
+
+# #---------------------------------------------------Test 3: Nodes join at later iterations---------------------------------------------------------------------------
+
+# echo "Running tests: Node joins at a later iteration"
+
+# for (( totalnodes = 3; totalnodes < 5; totalnodes++ )); do
 	
-	for (( outernode = 0; outernode < totalnodes; outernode++ )); do	
-		
-		outerLogFile=test3_$outernode\_$totalnodes.log
-		for (( innernode = 0; innernode < totalnodes ; innernode++ )); do
-			innerLogFile=test3_$innernode\_$totalnodes.log			
-			if [ $innerLogFile == $outerLogFile ]; then
-				continue
-			fi
-			if !(cmp -s $innerLogFile $outerLogFile) 
-			then
-				echo Test failed: $innerLogFile and $outerLogFile are different
-				echo "Files are different"
-				exit -1	
-			fi		
-		done
-	done
+# 	echo "Running with " $totalnodes "nodes"
 
-	echo "SUCCESS! Nodes have same blockchain"
+# 	for (( index = 0; index < (totalnodes); index++ )); do
+# 		thisLogFile=test3_$index\_$totalnodes.log
+# 		# sudo $GOPATH/bin/DistSys -i=$index -t=$totalnodes -d=creditcard > $thisLogFile & 
+# 		sudo $GOPATH/bin/DistSys -i=$index -t=$totalnodes -d=creditcard > $thisLogFile 2> /dev/null &
+# 		if [ $index -le 1 ] 
+# 		then
+# 			sleep 10
+# 		else
+# 			sleep 50			
+# 		fi
+# 	done	
 
-done
-
-#---------------------------------------------------Test 4: One node fails and stay failed---------------------------------------------------------------------------
-
-echo "Running tests: Node joins, and then fails midway"
-
-
-for (( totalnodes = 3; totalnodes < 5; totalnodes++ )); do
+# 	wait
+# 	echo "Running with " $totalnodes "nodes complete. Testing similarity of blockchain"
 	
-	echo "Running with " $totalnodes "nodes"
-
-	for (( index = 0; index < (totalnodes); index++ )); do
+# 	for (( outernode = 0; outernode < totalnodes; outernode++ )); do	
 		
-		thisLogFile=test4_$index\_$totalnodes.log
-		
-		# sudo $GOPATH/bin/DistSys -i=$index -t=$totalnodes -d=creditcard > $thisLogFile & 
-		sudo $GOPATH/bin/DistSys -i=$index -t=$totalnodes -d=creditcard > $thisLogFile 2> /dev/null &
-		sleep 10
+# 		outerLogFile=test3_$outernode\_$totalnodes.log
+# 		for (( innernode = 0; innernode < totalnodes ; innernode++ )); do
+# 			innerLogFile=test3_$innernode\_$totalnodes.log			
+# 			if [ $innerLogFile == $outerLogFile ]; then
+# 				continue
+# 			fi
+# 			if !(cmp -s $innerLogFile $outerLogFile) 
+# 			then
+# 				echo Test failed: $innerLogFile and $outerLogFile are different
+# 				echo "Files are different"
+# 				exit -1	
+# 			fi		
+# 		done
+# 	done
 
-	done
+# 	echo "SUCCESS! Nodes have same blockchain"
 
-	echo "All nodes $totalnodes online. Causing last node to fail"
+# done
 
-	let i=$totalnodes-1
-	let PORT=8000+$i
+# #---------------------------------------------------Test 4: One node fails and stay failed---------------------------------------------------------------------------
 
-	echo $PORT
-
-	sudo iptables -A INPUT -p tcp --destination-port $PORT -j REJECT
-	sudo iptables -A OUTPUT -p tcp --dport $PORT -j REJECT
-
-	wait
-
-	sudo iptables -F INPUT
-	sudo iptables -F OUTPUT
+# echo "Running tests: Node joins, and then fails midway"
 
 
-	echo "Running with " $totalnodes "nodes complete. Testing similarity of blockchain"
+# for (( totalnodes = 3; totalnodes < 5; totalnodes++ )); do
 	
-	for (( outernode = 0; outernode < totalnodes-1; outernode++ )); do	
+# 	echo "Running with " $totalnodes "nodes"
+
+# 	for (( index = 0; index < (totalnodes); index++ )); do
 		
-		outerLogFile=test4_$outernode\_$totalnodes.log
-		for (( innernode = 0; innernode < totalnodes-1 ; innernode++ )); do
-			innerLogFile=test4_$innernode\_$totalnodes.log			
-			if [ $innerLogFile == $outerLogFile ]; then
-				continue
-			fi
-			if !(cmp -s $innerLogFile $outerLogFile) 
-			then
-				echo Test failed: $innerLogFile and $outerLogFile are different
-				echo "Files are different"
-			fi		
-		done
-	done
+# 		thisLogFile=test4_$index\_$totalnodes.log
+		
+# 		# sudo $GOPATH/bin/DistSys -i=$index -t=$totalnodes -d=creditcard > $thisLogFile & 
+# 		sudo $GOPATH/bin/DistSys -i=$index -t=$totalnodes -d=creditcard > $thisLogFile 2> /dev/null &
+# 		sleep 10
 
-	echo "SUCCESS! Nodes have same blockchain"
+# 	done
 
-done
+# 	echo "All nodes $totalnodes online. Causing last node to fail"
+
+# 	let i=$totalnodes-1
+# 	let PORT=8000+$i
+
+# 	echo $PORT
+
+# 	sudo iptables -A INPUT -p tcp --destination-port $PORT -j REJECT
+# 	sudo iptables -A OUTPUT -p tcp --dport $PORT -j REJECT
+
+# 	wait
+
+# 	sudo iptables -F INPUT
+# 	sudo iptables -F OUTPUT
+
+
+# 	echo "Running with " $totalnodes "nodes complete. Testing similarity of blockchain"
+	
+# 	for (( outernode = 0; outernode < totalnodes-1; outernode++ )); do	
+		
+# 		outerLogFile=test4_$outernode\_$totalnodes.log
+# 		for (( innernode = 0; innernode < totalnodes-1 ; innernode++ )); do
+# 			innerLogFile=test4_$innernode\_$totalnodes.log			
+# 			if [ $innerLogFile == $outerLogFile ]; then
+# 				continue
+# 			fi
+# 			if !(cmp -s $innerLogFile $outerLogFile) 
+# 			then
+# 				echo Test failed: $innerLogFile and $outerLogFile are different
+# 				echo "Files are different"
+# 			fi		
+# 		done
+# 	done
+
+# 	echo "SUCCESS! Nodes have same blockchain"
+
+# done
