b510d9257e1d490df5bf0aa4418860ca83f45a30
Shayan <mshayanshafi@gmail.com>
2018-06-28 11:58:36 -0700

    Fixed runBiscotti Script to span over tow VMs

--- azure-deploy/runBiscotti.sh
+++ azure-deploy/runBiscotti.sh
@@ -3,10 +3,14 @@
 IFS=$'\n'
 set -f
 
+indexCount=0
+nodesInEachVM=10
+
 for line in $(cat tempHosts);do
 
 	tname=`echo $line | grep -E -o '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}'`	
 	# runScript=`deployNodes.sh 10 0 10 $tname`
-	ssh nss@$tname -x 'bash -s' < deployNodes.sh 20 0 10 $tname &
+	ssh nss@$tname -x 'bash -s' < deployNodes.sh 20 $indexCount $nodesInEachVM $tname &
+	indexCount=$indexCount+$nodesInEachVM
 
 done
