a4c9d1d86a32368d5ccbb2c7353456fa8479a0e7
Shayan <mshayanshafi@gmail.com>
2018-06-28 11:55:57 -0700

    Temp changes for scalability

--- azure-deploy/runBiscotti.sh
+++ azure-deploy/runBiscotti.sh
@@ -7,6 +7,6 @@ for line in $(cat tempHosts);do
 
 	tname=`echo $line | grep -E -o '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}'`	
 	# runScript=`deployNodes.sh 10 0 10 $tname`
-	ssh nss@$tname -x 'bash -s' < deployNodes.sh 10 0 10 $tname &
+	ssh nss@$tname -x 'bash -s' < deployNodes.sh 20 0 10 $tname &
 
 done
