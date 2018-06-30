261c25636f24c3af22e16a1ae977e89c75f930fb
Shayan <mshayanshafi@gmail.com>
2018-06-29 18:27:33 -0700

    Trying with 40 now

--- azure-deploy/runBiscotti.sh
+++ azure-deploy/runBiscotti.sh
@@ -6,7 +6,7 @@ set -f
 let indexCount=0
 let nodesInEachVM=10
 
-for line in $(cat hosts);do
+for line in $(cat tempHosts);do
 
 	tname=`echo $line | grep -E -o '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}'`	
 	# runScript=`deployNodes.sh 10 0 10 $tname`
