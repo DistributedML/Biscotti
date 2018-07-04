054242c0b1ad6b60d7cd4638fa0bfb7a4fcb5e10
Shayan <mshayanshafi@gmail.com>
2018-07-04 11:53:07 -0700

    Fixed minor bug regarding running locally

--- azure-deploy/runBiscotti.sh
+++ azure-deploy/runBiscotti.sh
@@ -11,7 +11,7 @@ for line in $(cat tempHosts);do
 	tname=`echo $line | grep -E -o '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}'`	
 	# runScript=`deployNodes.sh 10 0 10 $tname`
 	echo 
-	ssh nss@$tname -x 'bash -s' < deployNodes.sh $nodesInEachVM $indexCount 80 $tname &
+	ssh nss@$tname -x 'bash -s' < deployNodes.sh $nodesInEachVM $indexCount 40 $tname &
 	indexCount=$indexCount+$nodesInEachVM
 	# break
 
