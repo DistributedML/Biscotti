cff73b8cfd26e02af1ad65991f03182f3d204d42
Muhammad Shayan <shayan@naur.cs.ubc.ca>
2018-07-16 18:21:22 -0700

    Changed runBiscotti script to take input args

--- azure-deploy/runBiscotti.sh
+++ azure-deploy/runBiscotti.sh
@@ -4,16 +4,17 @@ IFS=$'\n'
 set -f
 
 let indexCount=0
-let nodesInEachVM=10
+let nodesInEachVM=$1
+let totalNodes=$2
 
 for line in $(cat tempHosts);do
 
 	tname=`echo $line | grep -E -o '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}'`
 
 	if [ "$tname" == "198.162.52.126" ]; then
-		ssh shayan@$tname 'bash -s' < deployNodes.sh $nodesInEachVM $indexCount 20 $tname &			
+		ssh shayan@$tname 'bash -s' < deployNodes.sh $nodesInEachVM $indexCount $totalNodes $tname &			
 	else
-		ssh cfung@$tname 'bash -s' < deployNodes.sh $nodesInEachVM $indexCount 20 $tname &
+		ssh cfung@$tname 'bash -s' < deployNodes.sh $nodesInEachVM $indexCount $totalNodes $tname &
 	fi
 
 	indexCount=$((indexCount + nodesInEachVM))
