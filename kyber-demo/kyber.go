2bf6465c2bb099cf92ad6b00184fb636b92db849
Muhammad Shayan <shayan@naur.cs.ubc.ca>
2018-08-21 18:00:23 -0700

    Public key API added, improved ShamirSecretAPI

--- kyber-demo/kyber.go
+++ kyber-demo/kyber.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	
-	"github.com/dedis/kyber/pairing/bn256"
-	"github.com/dedis/kyber/util/random"
+	"github.com/dedis/kyber/pairing/bn256"	
 	"github.com/dedis/kyber"
 	"fmt"
 	"crypto/sha256"
 	"encoding/binary"
 	"math"
+	"github.com/DzananGanic/numericalgo"
+	"github.com/DzananGanic/numericalgo/fit/poly"
 )
 
 var (
@@ -17,95 +18,143 @@ var (
 	precision = 4
 	maxPolynomialdegree = 10
 	maxXValue = 50 
+	totalShares = 10
 
 )
+
+
+type Share struct {
+	
+	x int64
+	y int64
+
+}
+
+// could be merged into one map
+
+type polynomialCommitment struct {
+
+	update  			[]float64
+	commitmentUpdate	kyber.Point
+	polynomialMap 		map[int][]int64
+	secretMap 			map[int][]Share
+	witnessMap			map[int][]Witness
+
+}
+
+
+
 
 func main() {
 	
 	suite = bn256.NewSuite()
 
-	seed := random.New()
-
-
 	deltaw := []float64{-0.010521993075977404,-0.01661607867341372,0.004996034130283805,-0.008155197968922704,0.008865055379273375,0.03812612569032836,0.02647248247415979,0.02278481207585922,0.02755816067341541,2.304,0.015136136175728015,0.010715115686446194,0.006171690049045976,0.010516143459950953,0.007760037020031965,0.00662185649157663,0.004544836244807698,0.004645047625730867,0.00016865983006748256,-0.008058385441209603,0.0023729020797505084,-0.007314085162704469,0.005490980060098083,0.0025286602301143562,0.15069781017821887,2.304}
 
-	numberOfDimensions := len(deltaw)
+	pkey := &PublicKey{}
 
-	publicKeyG1 := make([]kyber.Point , numberOfDimensions)
-	publicKeyG2 := make([]kyber.Point , numberOfDimensions)
+	pkey.GenerateKey(len(deltaw))
 
-	// generate secret key
-	privateKey := suite.G1().Scalar().Pick(seed)
-	fmt.Println("Private Key:" + privateKey.String()+ "\n")	
-	
-	// getting the generator for each group
+	fmt.Println(pkey)
 
-	generatorG1  := suite.G1().Point().Mul(suite.G1().Scalar().One(),nil)
-	generatorG2  := suite.G2().Point().Mul(suite.G2().Scalar().One(),nil)	
-	
-	fmt.Println("Generator Group G1:" + generatorG1.String()+ "\n")
-	fmt.Println("Generator Group G2:" + generatorG2.String()+ "\n")
-
-	previousKeyPartG1 := generatorG1
-	previousKeyPartG2 := generatorG2
-
-
-	for i := 0; i < (numberOfDimensions); i++ {
-		
-		publicKeyG1[i] = previousKeyPartG1
-		publicKeyG2[i] = previousKeyPartG2
-	
-		previousKeyPartG1 = suite.G1().Point().Mul(privateKey,previousKeyPartG1)	
-		previousKeyPartG2 = suite.G2().Point().Mul(privateKey,previousKeyPartG2)		
-	
-	}
-	
-	// Printing Public keys
-
-	fmt.Println("Public Key G1:"+ "\n")
-
-	for i := 0; i < numberOfDimensions; i++ {		
-	
-		fmt.Println(publicKeyG1[i].String()+ "\n")
-
-	}
-
-	fmt.Println("Public Key G2:"+ "\n")
-	for i := 0; i < numberOfDimensions; i++ {
-		
-		fmt.Println(publicKeyG2[i].String()+ "\n")
-	
-	}
+	minerKeys := generateNKeys(totalShares, len(deltaw))
 
 	updateInt := updateFloatToInt(deltaw, precision)
 
-	commitment, polynomialMap := createCommitment(updateInt[0:maxPolynomialdegree], publicKeyG1)
+	polynomialMap := makePolynomialMap(updateInt, maxPolynomialdegree)
 
-	fmt.Println("Update Commitment:",commitment) // Commitment to the complete update
+	prevIndex := 0
 
-	isCommitment := verifyCommitment(updateInt, publicKeyG1, commitment)
+	for index, subPolynomial := range polynomialMap{
 
-	fmt.Println(isCommitment)
+		commitment := createCommitment(subPolynomial, pkey.PKG1[prevIndex:index])
+		shares, witnesses :=  createSharesAndWitnesses(totalShares, subPolynomial, minerKeys, pkey.PKG1[prevIndex:index])		
+		prevIndex = index
 
-	secretValue, evaluatedValue, witness := createWitness(publicKeyG1[2],polynomialMap[0], publicKeyG1)
+		fmt.Println(commitment)
+		fmt.Println(shares)
+		fmt.Println(witnesses)
 
-	fmt.Println("X value: " , secretValue)
-	fmt.Println("Y value: ", evaluatedValue)
-	fmt.Println("Witness: ", witness)
+	}
 
-	isValidSecret := verifySecret(commitment, witness, publicKeyG2, publicKeyG1, publicKeyG2 , secretValue, evaluatedValue)
 
-	fmt.Println(isValidSecret)
+
+	// commitment:= createCommitment(updateInt[0:maxPolynomialdegree], publicKeyG1)
+
+	// fmt.Println("Update Commitment:",commitment) // Commitment to the complete update
+
+	// isCommitment := verifyCommitment(updateInt, publicKeyG1, commitment)
+
+	// fmt.Println(isCommitment)
+
+	// secretValue, evaluatedValue, witness := createWitness(publicKeyG1[2],polynomialMap[0], publicKeyG1)
+
+	// fmt.Println("X value: " , secretValue)
+	// fmt.Println("Y value: ", evaluatedValue)
+	// fmt.Println("Witness: ", witness)
+
+	// isValidSecret := verifySecret(commitment, witness, publicKeyG2, publicKeyG1, publicKeyG2 , secretValue, evaluatedValue)
+
+	// fmt.Println(isValidSecret)
 
 
 }
 
+func createSharesAndWitnesses(numberOfShares int, update []int64, minerKeys []PublicKey, pkeyG1 []kyber.Point) ([]Share, []kyber.Point) {
 
-func createCommitment(update []int64 , pkeyG1 []kyber.Point) (kyber.Point, map[int][]int64) {
+	shares := make([]Share, numberOfShares)
+	witnesses := make([]kyber.Point, numberOfShares)	
+
+	sharesPerMiner := numberOfShares/len(minerKeys)
+	minerIndex := -1
+	pubKeyIndex := 0
+
+	for i := 0; i < numberOfShares; i++ {
+
+		pubKeyIndex = i % sharesPerMiner 
+		
+		if (pubKeyIndex == 0) {
+			minerIndex++
+		}
+
+		x, y , witness := createShareAndWitness(minerKeys[minerIndex].PKG1[pubKeyIndex+1], update, pkeyG1)
+
+		thisShare := Share{x, y}
+
+		shares[i] = thisShare
+		witnesses[i] = witness
+				
+	}
+
+	return shares, witnesses
+
+
+}
+
+func generateNKeys(n int, length int) []PublicKey {
+
+	pkeys := make([]PublicKey, n)
+
+	for i := 0; i < n; i++ {
+	
+			pkey := PublicKey{}
+
+			pkey.GenerateKey(length)	
+
+			pkeys[i] = pkey
+	
+	}
+
+	return pkeys
+
+}
+
+
+func createCommitment(update []int64 , pkeyG1 []kyber.Point) (kyber.Point) {
 
 	//TODO: Insert check to see if length update and pubkey are okay.
 
-	updateInt := makePolynomialMap(update)
+	// updateInt := makePolynomialMap(update)
 
 	commitment := suite.G1().Point().Null()
 
@@ -114,27 +163,27 @@ func createCommitment(update []int64 , pkeyG1 []kyber.Point) (kyber.Point, map[i
 	commitmentParameter := suite.G1().Point().Null()
 
 	
-	for _, subPolynomial := range updateInt{
+	// for _, subPolynomial := range updateInt{
 
-		for i := 0; i < len(subPolynomial); i++ {
+	for i := 0; i < len(update); i++ {
+
+		parameterIntScalar = suite.G1().Scalar().SetInt64(update[i])
+
+		commitmentParameter = suite.G1().Point().Mul(parameterIntScalar,pkeyG1[i])
+
+		commitment.Add(commitment, commitmentParameter)
 	
-			parameterIntScalar = suite.G1().Scalar().SetInt64(subPolynomial[i])
+	}
 
-			commitmentParameter = suite.G1().Point().Mul(parameterIntScalar,pkeyG1[i])
+	// }	
 
-			commitment.Add(commitment, commitmentParameter)
-		
-		}
-
-	}	
-
-	return commitment, updateInt
+	return commitment
 
 }
 
 func verifyCommitment(updateInt []int64 , pkeyG1 []kyber.Point, committedUpdate kyber.Point) bool {
 
-	commitmentVal, _ := createCommitment(updateInt,pkeyG1)
+	commitmentVal := createCommitment(updateInt,pkeyG1)
 	
 	if (commitmentVal.Equal(committedUpdate)) {
 
@@ -147,7 +196,7 @@ func verifyCommitment(updateInt []int64 , pkeyG1 []kyber.Point, committedUpdate 
 	}
 }
 
-func createWitness(minerPubKey kyber.Point, update []int64, pkeyG1 []kyber.Point) (int64, int64, kyber.Point) {
+func createShareAndWitness(minerPubKey kyber.Point, update []int64, pkeyG1 []kyber.Point) (int64, int64, kyber.Point) {
 
 	updateFloat := make([]float64, len(update))
 
@@ -202,7 +251,7 @@ func createWitness(minerPubKey kyber.Point, update []int64, pkeyG1 []kyber.Point
 
 	// fmt.Println(qInt)
 	
-	witness, _ :=  createCommitment(qInt, pkeyG1)
+	witness :=  createCommitment(qInt, pkeyG1)
 
 	return minerSecretX, evaluatedValue, witness
 
@@ -270,7 +319,7 @@ func updateFloatToInt(update []float64, precision int) ([]int64) {
 
 }
 
-func makePolynomialMap(updateInt []int64) (map[int][]int64){
+func makePolynomialMap(updateInt []int64, maxPolynomialdegree int) (map[int][]int64){
 
 	// updateInt := updateFloatToInt(update, precision)
 
@@ -286,8 +335,8 @@ func makePolynomialMap(updateInt []int64) (map[int][]int64){
 
 		}
 
-		polynomialMap[i] = make([]int64, maxPolynomialdegree)		
-		polynomialMap[i] = updateInt[i:stopIndex]		
+		polynomialMap[stopIndex] = make([]int64, maxPolynomialdegree)		
+		polynomialMap[stopIndex] = updateInt[i:stopIndex]		
 		// fmt.Println(polynomialMap[i])
 
 	}
@@ -299,7 +348,7 @@ func makePolynomialMap(updateInt []int64) (map[int][]int64){
 
 }
 
-func updateIntToFloat(update []int64) []float64{
+func updateIntToFloat(update []int64, precision int) []float64{
 
 	updateFloat := make([]float64, len(update))
 
@@ -362,3 +411,26 @@ func degree(p []float64) int {
  
     return -1
 }
+
+func recoverSecret(x []int64, y []int64, degree int) []float64{
+
+	xFloat := updateIntToFloat(x,0) // standard int to float conversion
+
+	yFloat := updateIntToFloat(y,0)
+
+	xVector := numericalgo.Vector(xFloat)
+
+	yVector := numericalgo.Vector(yFloat)
+
+	mypolynomial := poly.New()
+
+	mypolynomial.Fit(xVector,yVector, degree)
+
+	aggregatedVectorInt := updateFloatToInt([]float64(mypolynomial.Coeff),0)
+
+	aggregatedVectorFloat := updateIntToFloat(aggregatedVectorInt, precision)
+
+	return aggregatedVectorFloat
+
+}
+
---
+++ kyber-demo/publicKey.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	
+	// "fmt"
+	// "strconv"
+	// "strings"
+	"github.com/dedis/kyber"
+	"github.com/dedis/kyber/pairing/bn256"
+	"github.com/dedis/kyber/util/random"
+
+	// "encoding/binary"
+	// "bytes"
+)
+
+// Update - data object representing a single update
+type PublicKey struct {
+	
+	PKG1 []kyber.Point
+	PKG2 []kyber.Point
+
+}
+
+// generates a public key based on a random seed
+
+func (pkey *PublicKey) GenerateKey(numberOfDimensions int) {
+
+	suite := bn256.NewSuite()
+
+	seed := random.New()
+
+	pkey.PKG1 = make([]kyber.Point , numberOfDimensions)
+	pkey.PKG2 = make([]kyber.Point , numberOfDimensions)
+
+	// generate secret key
+	privateKey := suite.G1().Scalar().Pick(seed)
+	// fmt.Println("Private Key:" + privateKey.String()+ "\n")	
+	
+	// getting the generator for each group
+
+	generatorG1  := suite.G1().Point().Mul(suite.G1().Scalar().One(),nil)
+	generatorG2  := suite.G2().Point().Mul(suite.G2().Scalar().One(),nil)	
+	
+	// fmt.Println("Generator Group G1:" + generatorG1.String()+ "\n")
+	// fmt.Println("Generator Group G2:" + generatorG2.String()+ "\n")
+
+	previousKeyPartG1 := generatorG1
+	previousKeyPartG2 := generatorG2
+
+
+	for i := 0; i < (numberOfDimensions); i++ {
+		
+		pkey.PKG1[i] = previousKeyPartG1
+		pkey.PKG2[i] = previousKeyPartG2
+	
+		previousKeyPartG1 = suite.G1().Point().Mul(privateKey,previousKeyPartG1)	
+		previousKeyPartG2 = suite.G2().Point().Mul(privateKey,previousKeyPartG2)		
+	
+	}
+
+
+}
+
+func (pkey *PublicKey) GetGeneratorG1() kyber.Point{
+
+	return pkey.PKG1[0]
+
+}
+
+func (pkey *PublicKey) GetGeneratorG2() kyber.Point{
+
+	return pkey.PKG2[0]
+
+}
+
+func (pkey *PublicKey) GetFirstPKG2() kyber.Point{
+
+	return pkey.PKG2[1]
+
+}
