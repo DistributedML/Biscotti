e9c8e7421f03efabb453b4c7ecbb486d76678988
Muhammad Shayan <shayan@naur.cs.ubc.ca>
2019-03-08 13:01:40 -0800

    Resolved vrf eval conflict

--- eval_vrf/runEval.sh
+++ eval_vrf/runEval.sh
@@ -1,8 +1,25 @@
+# Run with number of noisers, verifiers and aggregators
+# Dataset and deployment filename harcoded
+
+if [ "$#" -ne 3 ]; then
+
+    echo "Illegal number of parameters"
+    echo "usage:"
+    echo "[numNoisers, numVerifiers, numAggregators]"
+    exit
+
+fi
+
 azuredeployScript="$GOPATH/src/Biscotti/azure/azure-run"
 logFiles="$GOPATH/src/Biscotti/DistSys/LogFiles"
 
-filename="3_3_5.log"
+numNoisers=$1
+numVerifiers=$2
+numAggregators=$3
+
+filename=$numNoisers"_"$numVerifiers"_"5.log
+echo $filename
+
 cd $azuredeployScript
 bash runBiscotti.sh 5 100 hosts_diffDC mnist '-nn=3 -nv=3 -na=5'
-cd $logFiles
 mv log_0_100.log ../../eval_vrf/$filename
