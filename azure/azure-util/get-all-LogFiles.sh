d7417a72cf654009776dc5da219f4638d7418086
Muhammad Shayan <shayan@naur.cs.ubc.ca>
2019-03-08 13:13:46 -0800

    get logfiles merge

--- azure/azure-util/get-all-LogFiles.sh
+++ azure/azure-util/get-all-LogFiles.sh
@@ -1,10 +1,8 @@
-
-
-if [ "$#" -ne 3 ]; then
+if [ "$#" -ne 4 ]; then
 
     echo "Illegal number of parameters"
     echo "usage:"
-    echo "[username, destPath, hostFile]"
+    echo "[username, destPath, hostFile, DistSys/FedSys]"
     exit
 
 fi
@@ -12,6 +10,7 @@ fi
 username=$1
 destPath=$2
 ipfile=$3
+bisOrFed=$4
 
 ipfilepath='../azure-conf/'
 
@@ -23,7 +22,7 @@ for line in $(cat $hostFile);do
 
 	ipaddr=`echo $line | grep -E -o '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}'`
 
-	scp $username@$ipaddr:/home/$username/gopath/src/Biscotti/DistSys/LogFiles/*.log $destPath 
+	scp $username@$ipaddr:/home/$username/gopath/src/Biscotti/$bisOrFed/LogFiles/*.log $destPath
 
 done
 exit
