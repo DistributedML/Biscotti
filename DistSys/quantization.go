50902a646b06abc526e7adba2347650020489b36
stolet <matheusstolet@hotmail.com>
2019-03-29 12:09:02 -0700

    QuantizedWeights sruct

--- DistSys/quantization.go
+++ DistSys/quantization.go
@@ -2,6 +2,12 @@ package main
 
 import "math"
 
+type QuantizedWeights struct {
+	Weights []uint8
+	Min float64
+	Max float64
+}
+
 func findMinAndMax(weights []float64) (float64, float64) {
 	min := math.Inf(1)
 	max := math.Inf(-1)
@@ -16,7 +22,7 @@ func findMinAndMax(weights []float64) (float64, float64) {
 	return min, max
 }
 
-func quantizeWeights(weights []float64) []uint8 {
+func quantizeWeights(weights []float64) QuantizedWeights {
 	numOfIntervals := math.Pow(2, 8) - 1
 	min, max := findMinAndMax(weights)
 	quantizedWeights := make([]uint8, len(weights))
@@ -24,7 +30,7 @@ func quantizeWeights(weights []float64) []uint8 {
 		quantizedWeight := quantizeWeight(weight, numOfIntervals, min, max)
 		quantizedWeights[i] = quantizedWeight
 	}
-	return quantizedWeights
+	return QuantizedWeights{Weights: quantizedWeights, Min: min, Max: max}
 }
 
 func quantizeWeight(weight float64, numOfIntervals float64, min float64, max float64) uint8 {
@@ -34,10 +40,10 @@ func quantizeWeight(weight float64, numOfIntervals float64, min float64, max flo
 	return uint8(quantizedRepresentation)
 }
 
-func dequantizeWeights(quantizedWeights []uint8, min float64, max float64) []float64 {
-	weights := make([]float64, len(quantizedWeights))
-	for i, quantizedWeight := range quantizedWeights {
-		weights[i] = dequantizeWeight(quantizedWeight, min, max)
+func dequantizeWeights(quantizedWeights QuantizedWeights) []float64 {
+	weights := make([]float64, len(quantizedWeights.Weights))
+	for i, quantizedWeight := range quantizedWeights.Weights {
+		weights[i] = dequantizeWeight(quantizedWeight, quantizedWeights.Min, quantizedWeights.Max)
 	}
 	return weights
 }
