5850cf0f736a3601944afc98cc1e13b02404e929
Muhammad Shayan <shayan@naur.cs.ubc.ca>
2019-09-24 13:48:31 -0700

    Modified install script plus python requirements file

--- azure/azure-setup/azure-install.sh
+++ azure/azure-setup/azure-install.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 #install golang
-#go to home directory
+## go to home directory
 cd
 
-#download go binary
+# #download go binary
 wget https://dl.google.com/go/go1.10.3.linux-amd64.tar.gz
 
-#unzip and remove
+# # #unzip and remove
 sudo tar -C /usr/local -xzf go1.10.3.linux-amd64.tar.gz
 rm go1.10.3.linux-amd64.tar.gz
 
-#export path
+# # #export path
 export PATH=$PATH:/usr/local/go/bin
 echo "" >> .profile
 echo "#export go path" >> .profile
 echo "export PATH=$PATH:/usr/local/go/bin" >> .profile
 
-#make root directory and set GOPATH
+# # #make root directory and set GOPATH
 sudo rm -rf gopath
 mkdir -p gopath/src
 export GOPATH=$HOME/gopath
@@ -25,62 +25,51 @@ echo "" >> .profile
 echo "#set GOPATH" >> .profile
 echo "export GOPATH=$HOME/gopath" >> .profile
 
-#export workspace bin
+# # #export workspace bin
 export PATH=$PATH:$GOPATH/bin
 echo "" >> .profile
 echo "#set local bin" >> .profile
 echo "export PATH=$PATH:$GOPATH/bin" >> .profile
-#Go should be installed now
+# # #Go should be installed now
 
-# Pip doesn't work without this
+# # Pip doesn't work without this
 echo "export LC_ALL=C" >> .profile
 
-# #clone the TorMentor Repository
+# Install Biscotti
 echo "Installing Biscotti"
 cd gopath/src
 git clone https://github.com/DistributedML/Biscotti.git
 
-source ~/.profile
 
-#Install dependencies
-echo "Installing Dependencies"
+source ~/.bashrc
+
+# Install dependencies
+# echo "Installing Dependencies"
 go get github.com/DistributedClocks/GoVector/govec
 go get github.com/kniren/gota/dataframe
 go get github.com/sbinet/go-python
-go get gonum.org/v1/gonum/mat
 go get github.com/coniks-sys/coniks-go/crypto/vrf
 
-# Kyber and requirements
-go get github.com/dedis/kyber
+cp -a $HOME/gopath/src/Biscotti/lib/gonum $HOME/gopath/src/github.com
+cp -a $HOME/gopath/src/Biscotti/lib/dedis $HOME/gopath/src/github.com
+
 cd github.com/dedis/kyber
 go get -t ./... # install 3rd-party dependencies
 
-#Installing python libraries
+# source ~/.profile
+
+# # #Installing python libraries
+echo "Update"
+sudo apt-get update
 echo "pkg-config"
 sudo  apt-get install -y pkg-config
 echo "pip"
 sudo  apt-get install -y python-pip
-echo "pandas"
+echo "python-tk"
 sudo apt-get install -y python-tk
-echo "pandas"
-pip install pandas
-echo "emcee"
-pip install emcee
-echo "utils"
-pip install utils
-echo "torch"
-pip install torch
-pip install torchvision
-echo "sklearn"
-pip install sklearn
-pip install scipy
-# pip install scikit-image
 echo "skimage"
 sudo apt-get install -y python-skimage
-echo "mnist"
-pip install python-mnist
-echo "matplotlib"
-pip install 'matplotlib==2.2.2' --force-reinstall
+pip install -r requirements.txt
 
 cd $HOME/gopath/src/Biscotti/ML/Pytorch/data/mnist
 python parse_mnist.py
