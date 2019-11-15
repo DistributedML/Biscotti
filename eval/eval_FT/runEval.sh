7b754d7f526f0d1f00e19c11ce1e6ac4f39b52ac
Ubuntu <shayan@bis0.jigw1mx332kufcldjkzmoncr5g.bx.internal.cloudapp.net>
2019-11-15 03:30:27 +0000

    Change FT script

--- eval/eval_FT/runEval.sh
+++ eval/eval_FT/runEval.sh
@@ -1,39 +1,52 @@
 let converged=0
-dataset="creditcard"
-let numberOfNodes=50
+let churnRate=$1
+dataset=$2
+let numberOfNodes=$3
+
+sleepAfter=$((60/$churnRate))
+
+mkdir results
+
+echo $sleepAfter
+echo $dataset
+echo $numberOfNodes
 
 bisLocalPath="$GOPATH/src/Biscotti/DistSys/"
 
 cd $bisLocalPath 
-echo $dataset
-bash localTest.sh $numberOfNodes $dataset
+mkdir LogFiles
 
-# while [[ "$converged" -eq 0 ]]; do
+bash localTest.sh $numberOfNodes $dataset &
+
+# Allow all clients to come online
+sleep 15
+
+while [[ "$converged" -eq 0 ]]; do
 
 # 	#statements
-# 	let failNode=$((( RANDOM % numberOfNodes)))
+	let failNode=$((( RANDOM % numberOfNodes)))
 	
-# 	let vmCount=0
-# 	let failPort=$((failNode + 8000))
+	let vmCount=0
+	let failPort=$((failNode + 8000))
 	
-# 	echo $failNode
-# 	echo $failPort
+	echo $failNode
+	echo $failPort
 
-# 	if [[ "$failNode" -eq 0 ]]; then
-# 		echo I will not kill 0
-# 		continue
-# 	fi
+	if [[ "$failNode" -eq 0 ]]; then
+		echo "I will not kill 0"
+		continue
+	fi
          
-#     echo killing node 
-            
-# 	fuser -k $failPort/tcp
-			
-# 	sleep 5
+        echo "killing node"             
+	fuser -k $failPort/tcp
+	let failAfter=$(($sleepAfter - 5))
+	echo $failAfter 			
+	sleep $failAfter
 
-# 	echo "launching node"
+	echo "launching node"
+	./DistSys -i $failNode -t $numberOfNodes -d creditcard >> /dev/null &
+	sleep 5
 
-# 	./DistSys -i $failNode -t $numberOfNodes -d creditcard >> /dev/null &
+done
 
-# 	sleep 5
-
-# done
+# After killing the script
